292d4b806fca5ebc063ea91902b76db9a64ea636
Philip Imperato <philipimperato@gmail.com>
2021-09-17 15:20:11 -0400

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false,
+  mode: 'jit',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
